78db3e2361ae2d679e79325349e27083ea2df66c
Michal Nasiadka <michal.nasiadka@pl.ibm.com>
2017-02-17 10:56:32 +0100

    Added more stuff in snx-run

--- scripts/snx-run.sh
+++ scripts/snx-run.sh
@@ -5,7 +5,32 @@ DOCKER_SOURCE="mnasiadka/snx-dante"
 SNX_USER="EDITME"
 SNX_SERVER="EDITME"
 
-IFS= read -s  -p SNX Password: pwd
+while [[ $# -gt 0 ]]
+do
+key=$1
+
+case $key in
+    -l|--logs)
+    	LOGS="1"
+    	shift # past argument
+    ;;
+    -b|--browser)
+    	BROWSER="1"
+    	shift # past argument
+    ;;
+    -h|--help)
+	echo "Usage: --logs (shows docker logs after run) --browser (runs browser pointing to socks5)"
+	exit 0
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+IFS= read -s  -p "SNX Password:" pwd
+echo -e "\n" 
 
 if [ "$(docker ps -aq -f status=running -f name=$DOCKER_NAME)" ]; then
 	echo -ne "$DOCKER_NAME is running, stopping... "
@@ -20,4 +45,8 @@ fi
 echo -ne "Starting $DOCKER_NAME..."
 docker run -p 127.0.0.1:1080:1080 --cap-add=ALL -v /lib/modules:/lib/modules -v /dev:/dev -e SNX_PASSWORD=$pwd -e SNX_USER=$SNX_USER -e SNX_SERVER=$SNX_SERVER --name snx-dante -d -ti $DOCKER_SOURCE
 echo "done"
+
+if [ "$LOGS" ]; then
+	echo "LOGS:"
+	docker logs $DOCKER_NAME
 fi
